5577903afa8c24d0b58f6d22a25297707b702724
sarlmolchen <sarlmolchen@c81c12f4-6276-2f43-b71e-88467c5cecd4>
2019-04-18 07:24:39 +0000

    fix fall through
    git-svn-id: https://scm-gy.tencent.com/gzrd/gzrd_mail_rep/phoenix_proj/branches/phxqueue/phxqueue_dev@3040228 c81c12f4-6276-2f43-b71e-88467c5cecd4

--- phxqueue/comm/utils/hash_util.cpp
+++ phxqueue/comm/utils/hash_util.cpp
@@ -53,15 +53,15 @@ uint64_t MurmurHash64(const void *key, size_t len, uint64_t seed) {
 
     switch(len & 7)
     {
-    case 7: h ^= uint64_t(data2[6]) << 48;
-    case 6: h ^= uint64_t(data2[5]) << 40;
-    case 5: h ^= uint64_t(data2[4]) << 32;
-    case 4: h ^= uint64_t(data2[3]) << 24;
-    case 3: h ^= uint64_t(data2[2]) << 16;
-    case 2: h ^= uint64_t(data2[1]) << 8;
-    case 1: h ^= uint64_t(data2[0]);
-        h *= m;
+    case 7: h ^= uint64_t(data2[6]) << 48; break;
+    case 6: h ^= uint64_t(data2[5]) << 40; break;
+    case 5: h ^= uint64_t(data2[4]) << 32; break;
+    case 4: h ^= uint64_t(data2[3]) << 24; break;
+    case 3: h ^= uint64_t(data2[2]) << 16; break;
+    case 2: h ^= uint64_t(data2[1]) << 8; break;
+    case 1: h ^= uint64_t(data2[0]); break;
     };
+    h *= m;
 
     h ^= h >> r;
     h *= m;
